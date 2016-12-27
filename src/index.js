bd1544130168b48b6576d74ed53ff81ee91fdf56
Kadzuya Okamoto <arow.okamoto@gmail.com>
2016-12-27 13:21:00 +0900

    Make to set random numbers on load page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 "use strict";
 
+var setCol = function setCol(min, max, n, prefix) {
+  var cLength = max - min + 1;
+  var candidates = Array.from({ length: cLength }, function (_, i) {
+    return i + min;
+  });
+  Array.from({ length: n }).forEach(function (_, i) {
+    var idx = Math.floor(Math.random() * candidates.length);
+    var v = candidates[idx];
+    candidates.splice(idx, 1);
+    document.getElementById('js-cell-label-' + prefix + i).textContent = v;
+  });
+};
+
+setCol(1, 15, 5, 'B');
+setCol(16, 30, 5, 'I');
+setCol(31, 45, 4, 'N');
+setCol(46, 60, 5, 'G');
+setCol(61, 75, 5, 'O');
+document.getElementById('js-cell-label-Nfree').textContent = 'Free';
+
 require('./index.scss');
